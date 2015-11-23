ce78231a4c2164eb2e542b35e8f83f40aed8cfbd
Guest User <Guest@stk-cs01-ws06.westlothian.sch.uk>
2015-11-23 14:56:16 +0000

    1.1

--- PUPIL.java
+++ PUPIL.java
@@ -4,12 +4,12 @@ import java.io.*;               // for general file handling
 public class PUPIL
 {
     // array of MEMBER objects
-    private MEMBER memberList[];
+
     // number of members calculated after reading file
-    int noOfMembers;
+
     private String fName;
     private String sName;
-    private char mark;
+    private int mark;
 
     // CLASSes to open, create, read/write, close files
     FILEREADCSV markFile;        // to read file from storage
@@ -33,9 +33,10 @@ public class PUPIL
         fName = rowItems[0];
         sName = rowItems[1];
 
-        mark = rowItems[2];
+        mark = Integer.parseInt(rowItems[2]);
 
     }
+
     public String writeDetails()
     {
         // join up data into a string to output as a row
@@ -45,97 +46,34 @@ public class PUPIL
         memberData = memberData.concat(",");
         memberData = memberData.concat(sName);
         memberData = memberData.concat(",");
-        memberData = memberData.concat(mark);
+        memberData = memberData.concat(Integer.toString(mark));
         return memberData;
     }
 
-    public void setUpDataList()  {
-        // loop for each item in the array
-        for (int i=0; i<20; i++) {
-            //create a new DATA object and store in next array position
-            dataList[i] = new DATA();
-        }
-    }
-    // top level algorithm
-    public void processMembers()  throws IOException
-    {
-        setUpMemberList();
-        displayMembers();
-        countOKbmi();
-    }
-
-    private void setUpMemberList() throws IOException
-    {
-        // First user message
-        System.out.println("ScotFit Club: Membership BMI update/n");
-        System.out.println("** Preparing to read data file.");
-
-        // read file, fetch data as String array containing the rows
-        String[] dataRows = bmiFile.readCSVtable();
-        // calculate the number of member rows, skip headings
-        noOfMembers = dataRows.length - 1;
-
-        // update user with number of rows with membership details
-        System.out.println("** " + noOfMembers + " rows read.\n\n");
-
-        // prepare array for members
-        memberList = new MEMBER[noOfMembers];
-        // create member objects and copy data from source
-        for  (int i = 0; i < noOfMembers; i++) {
-            memberList[i] = new MEMBER();
-            // adjust to skip headings
-            memberList[i].readMemberDetails(dataRows[i+1]);
-        }
-    }
-
-    public void displayMembers() {
-        // Heading for the display
-        System.out.println("A listing of all applicants for the next year\n");
-        // results
-        for  (int i = 0; i < noOfMembers; i++) {
-            memberList[i].displayDetails();
-        }
-        // 2 blank line to separate this report from others.
-        System.out.print("\n\n\n");
-    }
-    // standard algorithm to find largest value, tracking POSITION of value
+  
     public void findMaxData()  {
         // choose position of first value
-        int maxDataPosition = 0;
+        int toppupil = 0;
 
         // repeat for the REST of the array
-     
-           for (int i=1; i<20; i++) {
+
+        for (int i=0; i<49; i++) {
 
             //compare current value with best value
-            if (dataList[i].getData() < dataList[maxDataPosition].getData()) {
+            //if (dataList[i].getData() < dataList[maxDataPosition].getData()) {
                 // update the position of the best value
-                maxDataPosition = i;
+            //    toppupil = mark;
             }
         }
     }
 
-        public static void main(String[] args)  throws IOException
-        {
-            PUPIL myPupil = new PUPIL();
-            myPupil.processMembers();
-        }
-    }
-
-    // populate the array with DATA objects
- //   public void setUpDataList()  {
-        // loop for each item in the array
-   //     for (int i=0; i<20; i++) {
-            //create a new DATA object and store in next array position
-    //        dataList[i] = new DATA();
-   //     }
+  //  public static void main(String[] args)  throws IOException
+   // {
+     //   PUPIL myPupil = new PUPIL();
+    //    myPupil.processMembers();
    // }
-
-    //display results: position and the best (max) value
-   // System.out.print("Position is:" + minDataPosition + " , Value is:");
-  //  dataList[minDataPosition].displayData();
-  //  System.out.println();
 //}
+
 
 //}
 
---
+++ TOPPUPIL.java
@@ -0,0 +1,13 @@
+
+import javax.swing.JOptionPane;
+import java.io.*;               // for general file handling
+public class TOPPUPIL
+{
+    public TOPPUPIL()
+    {
+    
+}
+public void processpupil()
+{
+}
+}
